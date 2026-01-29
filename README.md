37f93c2d285621a1540fdbfe007f10605bcab0e4
Jan Luca Oster <janluca.oster@proton.me>
2026-01-29 11:52:24 +0100

    git init

---
+++ README.md
@@ -0,0 +1,1 @@
+A simple DBus interface for Playerctld.
---
+++ src/lib.rs
@@ -0,0 +1,21 @@
+use zbus::{Result, proxy};
+
+#[proxy(
+    interface = "com.github.altdesktop.playerctld",
+    default_service = "org.mpris.MediaPlayer2.playerctld",
+    default_path = "/org/mpris/MediaPlayer2"
+)]
+trait Playerctld {
+	#[zbus(signal)]
+    fn active_player_change_begin(&self, name: String) -> zbus::Result<()>;
+
+    #[zbus(signal)]
+    fn active_player_change_end(&self, name: String) -> zbus::Result<()>;
+
+	#[zbus(property)]
+    fn player_names(&self) -> Result<Vec<String>>;
+
+    fn shift(&self) -> Result<String>;
+
+    fn unshift(&self) -> Result<String>;
+}
